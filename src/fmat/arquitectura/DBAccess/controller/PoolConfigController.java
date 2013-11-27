fb7991b201ec8c0fb18869d85aaa628c120ac4fd
bheftye <bheftye92@gmail.com>
2013-11-27 17:06:54 -0600

    Servicio getPoolConfigInfo de PoolConfigController
    Servicio de Obtención del PoolConfigInfo.

--- src/fmat/arquitectura/DBAccess/controller/PoolConfigController.java
+++ src/fmat/arquitectura/DBAccess/controller/PoolConfigController.java
@@ -1,10 +1,18 @@
 package fmat.arquitectura.DBAccess.controller;
 
+import fmat.arquitectura.DBAccess.modelo.PoolConfigInfo;
+
 public class PoolConfigController {
 	
-	public PoolConfigController() {
-		// TODO Auto-generated constructor stub
-	}
-
+		private static PoolConfigController poolConfigController = new PoolConfigController();
+		private PoolConfigController(){}
+	
+		public PoolConfigController getPoolConfigController(){
+			return poolConfigController;
+		}
+		
+		public PoolConfigInfo getPoolConfigInfo(){
+			return new PoolConfigInfo();
+		}
 	
 }
