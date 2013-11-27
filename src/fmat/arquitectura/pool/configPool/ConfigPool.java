a9e0cea6f02ec8dce47e397a3dda34ffa36acc3b
Jeans Michell <licona.dorantes@gmail.com>
2013-11-27 16:52:41 -0600

    Singleton ConfigPool

--- src/fmat/arquitectura/pool/configPool/ConfigPool.java
+++ src/fmat/arquitectura/pool/configPool/ConfigPool.java
@@ -2,18 +2,28 @@ package fmat.arquitectura.pool.configPool;
 
 import fmat.arquitectura.DBAccess.modelo.PoolConfigInfo;
 import fmat.arquitectura.pool.AdminPool.GestorPool.Controlador_Pool;
+import fmat.arquitectura.pool.AdminPool.dominio.Pool;
 
 import java.util.Arrays;
 
 public class ConfigPool {
  
 	private Controlador_Pool controladorPool;
-	 
-	 
+	private static ConfigPool INSTANCE=createInstance();
+	
+	public static  ConfigPool  getInstance() {
+        return INSTANCE;
+    } 
+	
+	
 	public boolean notificarCambioPool(PoolConfigInfo datosConfig) {
 		return false;
 	}
 	 
+	private synchronized static ConfigPool createInstance() {
+		INSTANCE = new ConfigPool();
+		return INSTANCE;
+    }
 	
 }
  
